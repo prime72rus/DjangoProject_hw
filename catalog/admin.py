1421316c9f6b9693acca95bc239b0a05d2460e9f
Dmitrii Grechko <prime.optimys@mail.ru>
2025-04-23 15:10:13 +0500

    create superuser register models in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "category_name",)
+    search_fields = ("category_name", "category_description",)
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "product_name", "price", "category",)
+    search_fields = ("product_name", "product_description",)
+    list_filter = ("category",)
