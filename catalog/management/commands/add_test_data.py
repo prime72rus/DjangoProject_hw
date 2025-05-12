cc389325adc2ccc21d195fe5aae2d44ba358468e
Dmitrii Grechko <prime.optimys@mail.ru>
2025-05-12 21:18:01 +0500

    add custom command add_test_data.py

---
+++ catalog/management/commands/add_test_data.py
@@ -0,0 +1,18 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    help = 'Add test data to the database'
+
+    def handle(self, *args, **kwargs):
+        Category.objects.all().delete()
+
+        call_command('loaddata', 'catalog/fixtures/category_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
+
+        Product.objects.all().delete()
+
+        call_command('loaddata', 'catalog/fixtures/product_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
