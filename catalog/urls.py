14eaf29be60c8a8b5c068980516a8bb9977aff75
Dmitrii Grechko <prime.optimys@mail.ru>
2025-05-29 20:53:38 +0500

    add forms.py edit views.py and templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ContactsView, ProductCreateView
+from catalog.views import ProductListView, ProductDetailView, ContactsView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 
 app_name = CatalogConfig.name
@@ -9,5 +9,7 @@ urlpatterns = [
     path("home/", ProductListView.as_view(), name="home"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
     path("product_detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
-    path("product_create/", ProductCreateView.as_view(), name="product_create")
+    path("product_create/", ProductCreateView.as_view(), name="product_create"),
+    path("product_update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
+    path("product_delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
 ]
